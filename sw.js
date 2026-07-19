a5c6bf13e6344d3e3fbad39e4b0bebc96234f965
fxf-199 <fxf-199@users.noreply.github.com>
2026-07-19 09:00:59 +0800

    v10: 修自动更新死循环 + weakStreak改SM-2退2级 + 模糊纯当天 + 薄弱间隔打折x0.6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ebbinghaus-v8',URLS=['./','./index.html','./manifest.json','./icon.svg','./version.json'];
+const CACHE='ebbinghaus-v10',URLS=['./','./index.html','./manifest.json','./icon.svg','./version.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(URLS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim();});
 // Network-First：网络优先，离线回退缓存
